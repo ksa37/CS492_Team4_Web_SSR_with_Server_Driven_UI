cf0eb906aca5e526035de0c931ddfe37c73cfa31
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-10 01:45:24 +0900

    Add parameters in card component and pass data

--- src/components/card.js
+++ src/components/card.js
@@ -24,39 +24,61 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 //     )
 // }
 
-export default function Card() {    
+export default function Card({props}) {  
+    const { publisherURL, publisherImgURL, publisher, date, newsURL, title, contents, contentsImgURL } = props
+    // const data = { 
+    //     "publisherURL": "http://www.btnnews.tv",
+    //     "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
+    //     "publisher": "BTN불교TV",
+    //     "date": "7시간 전",
+    //     "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
+    //     "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
+    //     "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
+    //     "contentsImgURL": "/images/news_images/BTN_1.jpg"
+    // }
+
     return (
         <div className={styles.postOutline}> 
             <div className={styles.post}> 
                 <div className={styles.cardHeader}>
                     <div className={styles.publishInfo}>
-                        <Link href="http://www.btnnews.tv">
+                        {/* <Link href="http://www.btnnews.tv"> */}
+                        {/* <Link href={data.publisherURL}> */}
+                        <Link href={publisherURL}>
                             <a>
                             <div className={styles.publishInfo}>
                                 <Image className={styles.publisherImg}
-                                    src='/images/news_publisher_logos/BTN.jpg'
+                                    // src='/images/news_publisher_logos/BTN.jpg'
+                                    src={publisherImgURL}
                                     width={24}
                                     height={24}
                                     layout='fixed'
                                     objectFit='contain'
                                 />
-                                <div className={styles.publisher}>BTN불교TV</div>
+                                {/* <div className={styles.publisher}>BTN불교TV</div> */}
+                                {/* <div className={styles.publisher}>{data.publisher}</div> */}
+                                <div className={styles.publisher}>{publisher}</div>
                             </div>
                             </a>
                         </Link>
-                        <hr className={styles.verticalDivider}></hr>
+                        {/* <hr className={styles.verticalDivider}></hr> */}
                         <div className={styles.date}>7시간 전</div>
+                        {/* <div className={styles.date}>{date}</div> */}
                     </div>
                     <MoreVertIcon className={styles.morevert}>
                     </MoreVertIcon>
                 </div>
                 <Link href="http://m.btnnews.tv/news/articleView.html?idxno=68717">
+                {/* <Link href={newsURL}> */}
                     <a>
                         <div className={styles.title}>불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행</div>
+                        {/* <div className={styles.title}>{title}</div> */}
                         <div className={styles.contentsInfo}>
                             <div className={styles.contents}>신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...</div>
+                            {/* <div className={styles.contents}>{contents}</div> */}
                             <Image className={styles.contentsImg}
                                 src="/images/news_images/BTN_1.jpg"
+                                // src={contentsImgURL}
                                 width={88}
                                 height={88}
                                 layout='fixed'
--- src/pages/news.js
+++ src/pages/news.js
@@ -3,7 +3,59 @@ import Link from 'next/link'
 import Card from '../components/card'
 import styles from './news.module.css'
 
-export default function FirstPost() {
+const dummydata = {
+    "keywords": [ {
+                    "keyword": "불국사",
+                    "view": [ "basicInfo", "photo", "news" ],
+                    "basicInfo": {
+                                    "지정종목": "사적",
+                                    "지정번호": "제502호" 
+                                },
+                    "photo": [ "url_1", "url_2", "url_3" ],
+                    "news": [
+                                { 
+                                    "publisherURL": "http://www.btnnews.tv",
+                                    "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
+                                    "publisher": "BTN불교TV",
+                                    "date": "7시간 전",
+                                    "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
+                                    "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
+                                    "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
+                                    "contentsImgURL": "/images/news_images/BTN_1.jpg"
+                                },
+                                { 
+                                    "publisherURL": "http://www.btnnews.tv",
+                                    "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
+                                    "publisher": "BTN불교TV",
+                                    "date": "7시간 전",
+                                    "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
+                                    "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
+                                    "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
+                                    "contentsImgURL": "/images/news_images/BTN_1.jpg"
+                                }
+                            ]
+                        },
+        {
+            "keyword": "모가디슈"
+
+        }
+    ]
+}
+
+const newsdummy = [
+    { 
+        "publisherURL": "http://www.btnnews.tv",
+        "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
+        "publisher": "BTN불교TV",
+        "date": "7시간 전",
+        "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
+        "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
+        "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
+        "contentsImgURL": "/images/news_images/BTN_1.jpg"
+    }
+]
+
+export default function News() {
     return (
         <>
             <Head>
@@ -11,7 +63,7 @@ export default function FirstPost() {
             </Head>
             <div className={styles.news}>
                 <hr className={styles.divider}></hr>
-                <div className={styles.newsHeader}>
+                <div className={styles.viewHeader}>
                     <div className={styles.viewName}>뉴스</div>
                     <div className={styles.viewOptions}>
                         <div className={styles.viewSort}>관련도순</div>
@@ -20,10 +72,13 @@ export default function FirstPost() {
                 </div>
                 <hr className={styles.divider}></hr>
             </div>
+            {/* {dummydata["keywords"][0]["news"][0]["publisherURL"]} */}
+            {/* {newsdummy.map((news) => <Card props={news}/>)} */}
+            {dummydata["keywords"][0]["news"].map((news) => <Card props={news}/>)}
+            {/* <Card props = {dummydata["keywords"][0]["news"][0]}></Card> */}
+            {/* <Card></Card>
             <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <Card></Card> */}
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
