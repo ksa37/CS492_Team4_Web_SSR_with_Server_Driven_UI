01d4c0521a061936b8efa3449ca7602f232ea114
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-10 02:02:42 +0900

    Delete unnecessary codes

--- src/components/card.js
+++ src/components/card.js
@@ -5,37 +5,8 @@ import Image from "next/image"
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 
-// export default function Card(props) {    
-//     return (
-//         <div className="post" key={props.id}> 
-//             {(props.publisher_image === "" || props.publisher_image === null) ? 
-//             <br></br>
-//             // : <img src={props.publisher_image} width="100%" alt="shared-diary-post-card" />}
-//             : <img src={props.publisher_image} width="100%" />}
-//             <div className="publisher">{props.publisher}</div>
-//             <div className="date">{props.date}</div>
-//             <div className="title">{props.title}</div>
-//             <div className="contents">{props.contents}</div>
-//             <br></br>
-//             {(props.image === "" || props.image === null) ? 
-//             <br></br>
-//             : <img src={props.image} width="100%"/>}
-//         </div>
-//     )
-// }
-
 export default function Card({props}) {  
     const { publisherURL, publisherImgURL, publisher, date, newsURL, title, contents, contentsImgURL } = props
-    // const data = { 
-    //     "publisherURL": "http://www.btnnews.tv",
-    //     "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
-    //     "publisher": "BTN불교TV",
-    //     "date": "7시간 전",
-    //     "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
-    //     "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
-    //     "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
-    //     "contentsImgURL": "/images/news_images/BTN_1.jpg"
-    // }
 
     return (
         <div className={styles.postOutline}> 
@@ -43,7 +14,6 @@ export default function Card({props}) {
                 <div className={styles.cardHeader}>
                     <div className={styles.publishInfo}>
                         {/* <Link href="http://www.btnnews.tv"> */}
-                        {/* <Link href={data.publisherURL}> */}
                         <Link href={publisherURL}>
                             <a>
                             <div className={styles.publishInfo}>
@@ -56,29 +26,28 @@ export default function Card({props}) {
                                     objectFit='contain'
                                 />
                                 {/* <div className={styles.publisher}>BTN불교TV</div> */}
-                                {/* <div className={styles.publisher}>{data.publisher}</div> */}
                                 <div className={styles.publisher}>{publisher}</div>
                             </div>
                             </a>
                         </Link>
-                        {/* <hr className={styles.verticalDivider}></hr> */}
-                        <div className={styles.date}>7시간 전</div>
-                        {/* <div className={styles.date}>{date}</div> */}
+                        <hr className={styles.verticalDivider}></hr>
+                        {/* <div className={styles.date}>7시간 전</div> */}
+                        <div className={styles.date}>{date}</div>
                     </div>
                     <MoreVertIcon className={styles.morevert}>
                     </MoreVertIcon>
                 </div>
-                <Link href="http://m.btnnews.tv/news/articleView.html?idxno=68717">
-                {/* <Link href={newsURL}> */}
+                {/* <Link href="http://m.btnnews.tv/news/articleView.html?idxno=68717"> */}
+                <Link href={newsURL}>
                     <a>
-                        <div className={styles.title}>불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행</div>
-                        {/* <div className={styles.title}>{title}</div> */}
+                        {/* <div className={styles.title}>불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행</div> */}
+                        <div className={styles.title}>{title}</div>
                         <div className={styles.contentsInfo}>
-                            <div className={styles.contents}>신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...</div>
-                            {/* <div className={styles.contents}>{contents}</div> */}
+                            {/* <div className={styles.contents}>신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...</div> */}
+                            <div className={styles.contents}>{contents}</div>
                             <Image className={styles.contentsImg}
-                                src="/images/news_images/BTN_1.jpg"
-                                // src={contentsImgURL}
+                                // src="/images/news_images/BTN_1.jpg"
+                                src={contentsImgURL}
                                 width={88}
                                 height={88}
                                 layout='fixed'
@@ -87,7 +56,6 @@ export default function Card({props}) {
                         </div>
                     </a>
                 </Link>
-                {/* <Divider horizontal></Divider> */}
             </div>
             <hr className={styles.divider}></hr>
 
--- src/pages/news.js
+++ src/pages/news.js
@@ -24,13 +24,13 @@ const dummydata = {
                                     "contentsImgURL": "/images/news_images/BTN_1.jpg"
                                 },
                                 { 
-                                    "publisherURL": "http://www.btnnews.tv",
+                                    "publisherURL": "https://media.naver.com/press/009",
                                     "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
-                                    "publisher": "BTN불교TV",
-                                    "date": "7시간 전",
-                                    "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
-                                    "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
-                                    "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
+                                    "publisher": "매일경제",
+                                    "date": "2일 전",
+                                    "newsURL": "https://n.news.naver.com/mnews/article/009/0004873767?sid=103",
+                                    "title": "세계 1위 여행출판사 '경주' 콕 찍었다 코로나 풀리면 꼭 가라",
+                                    "contents": "불국사와 석굴암 등 주요 유적지에 대해서도 언급하며 경주를 충분히 즐기기 위해서는 며칠 간 머물러야 한다고 이 책은 조언했다. 론리 플래닛은 이번에 독일의 '환경수도' 프라이부르크(3위), 미국 남부 중심도시 애틀랜타(4위), 멕시코 유카탄주 최대도시 메리다(8위), 유럽 근대 르네상스의...",
                                     "contentsImgURL": "/images/news_images/BTN_1.jpg"
                                 }
                             ]
@@ -41,19 +41,6 @@ const dummydata = {
         }
     ]
 }
-
-const newsdummy = [
-    { 
-        "publisherURL": "http://www.btnnews.tv",
-        "publisherImgURL": "/images/news_publisher_logos/BTN.jpg",
-        "publisher": "BTN불교TV",
-        "date": "7시간 전",
-        "newsURL": "http://m.btnnews.tv/news/articleView.html?idxno=68717",
-        "title": "불국사, 신라불교 최초 공인한 법흥왕 기려..다례재 봉행",
-        "contents": "신라시대 불교를 공인한 법흥왕을 기리는 추모 다례재가 오늘 불국사 신도회 주최로 경주 코모도 호텔 잔디광장에서 봉행됐습니다. 이영숙 / 불국사 신도회장 (신라불교를 최초 공인한 임금으로서 우리들은 그 동안 그 크신 공헌을 까맣게 잊고 살아오지 않았나 반성하며...",
-        "contentsImgURL": "/images/news_images/BTN_1.jpg"
-    }
-]
 
 export default function News() {
     return (
@@ -75,10 +62,6 @@ export default function News() {
             {/* {dummydata["keywords"][0]["news"][0]["publisherURL"]} */}
             {/* {newsdummy.map((news) => <Card props={news}/>)} */}
             {dummydata["keywords"][0]["news"].map((news) => <Card props={news}/>)}
-            {/* <Card props = {dummydata["keywords"][0]["news"][0]}></Card> */}
-            {/* <Card></Card>
-            <Card></Card>
-            <Card></Card> */}
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
