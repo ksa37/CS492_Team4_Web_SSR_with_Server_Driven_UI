4693d86a354c4fadf8b5902fd1e00c3c228b1de0
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-10 16:15:37 +0900

    Add components that uses material UI made by 수아

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Card from '../components/card'
+// import Card from '../components/card'
 import styles from './news.module.css'
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Chip, CssBaseline, Divider, Grid, IconButton, Paper, Stack, Typography} from '@mui/material';
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+var imgA = "/images/news_publisher_logos/BTN.jpg";
 
 const dummydata = {
     "keywords": [ {
@@ -42,29 +47,199 @@ const dummydata = {
     ]
 }
 
+const itemData = [
+    {
+      img: imgA,
+      tag_name: 'Breakfast',
+    },
+    {
+      img: imgA,
+      tag_name: 'Burger',
+    },
+    {
+      img: imgA,
+      tag_name: 'Camera',
+    },
+    {
+      img: imgA,
+      tag_name: 'Coffee',
+    },
+    {
+      img: imgA,
+      tag_name: 'Hats',
+    },
+    {
+      img: imgA,
+      tag_name: 'Honey',
+    },
+    {
+      img: imgA,
+      tag_name: 'Basketball',
+    },
+    {
+      img: imgA,
+      tag_name: 'Fern',
+    },
+    {
+      img: imgA,
+      tag_name: 'Mushrooms',
+    },
+  
+  ];
+  
+
+// export default function News() {
+//     return (
+//         <>
+//             <Head>
+//                 <title>news</title>
+//             </Head>
+//             <div className={styles.news}>
+//                 <hr className={styles.divider}></hr>
+//                 <div className={styles.viewHeader}>
+//                     <div className={styles.viewName}>뉴스</div>
+//                     <div className={styles.viewOptions}>
+//                         <div className={styles.viewSort}>관련도순</div>
+//                         <div className={styles.viewSort}>최신순</div>
+//                     </div>
+//                 </div>
+//                 <hr className={styles.divider}></hr>
+//             </div>
+//             {dummydata["keywords"][0]["news"].map((news) => <Card props={news}/>)}
+//             <h2>
+//                 <Link href="/">
+//                     <a>Back to home</a>
+//                 </Link>
+//             </h2>
+//         </>
+//     )
+// }
+
+const card_header_date = 
+<Typography display="inline" style={{fontSize: 16}} > 
+  awesome days{' '} | 5일 전
+</Typography>;
+
+const card_header_title = 
+<Typography variant="h2" display="inline" style={{fontSize: 17}} > 
+  <b>이미지</b>
+</Typography>;
+
 export default function News() {
-    return (
-        <>
-            <Head>
-                <title>news</title>
-            </Head>
-            <div className={styles.news}>
-                <hr className={styles.divider}></hr>
-                <div className={styles.viewHeader}>
-                    <div className={styles.viewName}>뉴스</div>
-                    <div className={styles.viewOptions}>
-                        <div className={styles.viewSort}>관련도순</div>
-                        <div className={styles.viewSort}>최신순</div>
-                    </div>
-                </div>
-                <hr className={styles.divider}></hr>
-            </div>
-            {dummydata["keywords"][0]["news"].map((news) => <Card props={news}/>)}
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
-    )
+  return (
+    <>
+      <CssBaseline />
+    
+    <div className="App" >
+      <Grid 
+        container
+        justifyContent="center"
+        alignItems="center"
+        direction="column" 
+      >
+
+      <div className="section_view">
+        <Card sx={{ maxWidth: 766}} variant='outlined' square > 
+          <CardHeader
+            avatar={
+              <Avatar src="/images/news_publisher_logos/BTN.jpg" alt="awesome days" />
+            
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title = {card_header_date}
+          
+            style={{ textAlign: 'left', height:48}}
+          />
+          <CardContent>
+            <Typography variant="body1" gutterBottom align="left">
+              [프랑스 여행 D+7] 파리 여행 마지막날 간 곳들(팡테온,튈르리정원, 퐁피두센터)
+            </Typography>
+            <Typography variant="body2" color="text.secondary" align="left">
+            여행기간 : 170603-170610 프랑스여행/파리여행/파리볼거리/파리관광지/팡테온/튈르리정원/퐁피두센터 어느덧 여행의 마지막날. 
+            여행의 마지막날은 언제나 울적하다.ㅠㅠ 그래서 더욱...
+            </Typography>
+            <CardMedia
+            component="img"
+            sx={{
+              width:87,
+              height:87,
+            }}
+            image="/images/news_images/BTN_1.jpg"
+            alt="[프랑스 여행 D+7] 파리 여행 마지막날 간 곳들(팡테온,튈르리정원, 퐁피두센터)"
+            />
+            <Stack direction="row" spacing={1} m={2} pt={3}>
+              <Chip label="#파리여행" component="a" href="#basic-chip" clickable />
+              <Chip
+                label="#파리관광지"
+                component="a"
+                href="#basic-chip"
+                variant="outlined"
+                clickable
+              />
+            </Stack>
+          </CardContent>                      
+        
+          <Divider />
+          {/* <hr/> */}
+          <CardActions disableSpacing>
+            {/* <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+              <ExpandMoreIcon /> */}
+          </CardActions>
+        </Card>
+      </div>
+
+
+      {/* <div className="section_image">
+        <Card sx={{ maxWidth: 766}} variant='outlined' square>
+          <CardHeader 
+            className="api_title_area"
+            title= {card_header_title}
+            style={{ textAlign: 'left'}}
+          />
+          <Paper 
+            className="group_option_tag_wrap" 
+            style={{height: 83, overflow: 'auto', backgroundColor: '#f5f7f8'}}
+          >
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              style={{minHeight:66}}
+             
+            >
+            <Stack direction="row" spacing={1} style= {{paddingLeft:12, paddingRight:12}}>
+            {itemData.map((item) => (
+              <Chip 
+                key={item.tag_name}
+                avatar={<Avatar src={item.img} />}
+                label={item.tag_name}
+                variant="outlined"
+                style={{backgroundColor: "#ffffff"}}
+              />
+            ))}
+            </Stack>
+            </Grid>
+          </Paper>
+          {/* <StandardImageList /> */}
+          {/* <Paper style={{height: 50, overflow: 'auto', backgroundColor: '#f4f7f8'}}>
+            <Typography style={{height:48, fontSize: 16, verticalAlign:'center'}}>
+              이미지 더보기 <ArrowForwardIcon sx={{mt:2}} style={{height: 16, width: 20}}/>
+            </Typography>
+          </Paper>
+        </Card>
+      </div>   */}
+      </Grid>
+    </div>
+    </>
+    
+  );
 }
