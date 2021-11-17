eab86291a7fb4df27a0fa26480197bf0784c1e20
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-17 16:07:18 +0900

    Add news data in data.json

--- render_server/src/components/StandardImageList.js
+++ render_server/src/components/StandardImageList.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import Divider from '@mui/material/Divider';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
 
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Image from "next/image"
+
 
 export default function StandardImageList({props}) {
 
