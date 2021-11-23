0c1cc78c4574d7151b48e717cc17b10e3a1729fe
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-24 02:10:20 +0900

    Done with news sorting without css

--- render_server/next.config.js
+++ render_server/next.config.js
@@ -1,5 +1,16 @@
+// module.exports = {
+//   reactStrictMode: true,
+//   test: /\.svg$/,
+//   use: ['@svgr/webpack'],
+// }
+
 module.exports = {
-  reactStrictMode: true,
-  test: /\.svg$/,
-  use: ['@svgr/webpack'],
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+};
