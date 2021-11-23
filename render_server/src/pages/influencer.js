02a805eabf2a8fbdfe916225cab858def8de8698
Zieho Choi <jeff426@naver.com>
2021-11-23 15:40:46 +0900

    influencer tb merged w develop folder

--- render_server/src/pages/influencer.js
+++ render_server/src/pages/influencer.js
@@ -1,33 +1,18 @@
 import Container from '@mui/material/Container';
-// import Grid from '@mui/material/Grid';
-// import ImageList from '@mui/material/ImageList';
-// import ImageListItem from '@mui/material/ImageListItem';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 // import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 import styles from './influencer.module.css'
 
-// require('react-dom');
-// window.React2 = require('react');
-// console.log(window.React1 === window.React2);
-
 function addblogfan() {
-  // const [login, setloggedin] = useState('false');
   var answer = window.confirm("로그인이 필요한 서비스입니다.\n로그인 하시겠습니까?");
   if (answer) {
     window.location.href = "https://nid.naver.com/nidlogin.login?svctype=262144&url=https%3A%2F%2Fsearch.naver.com%2Fsearch.naver%3Fwhere%3Dnexearch%26sm%3Dtop_hty%26fbm%3D1%26ie%3Dutf8%26query%3D%25EB%25B6%2588%25EA%25B5%25AD%25EC%2582%25AC"
-      // if (login) {
-      //   console.log('찜하기 기능 실행')
-      // }
-      // else {window.location.href = "https://nid.naver.com/nidlogin.login?svctype=262144&url=https%3A%2F%2Fsearch.naver.com%2Fsearch.naver%3Fwhere%3Dnexearch%26sm%3Dtop_hty%26fbm%3D1%26ie%3Dutf8%26query%3D%25EB%25B6%2588%25EA%25B5%25AD%25EC%2582%25AC";
-      // }
 }
   else {}
 }
@@ -124,22 +109,31 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'left',
 }));
 
-const Search = () => {
+const Search = (props) => {
   const [showResults, setShowResults] = useState(false)
   const onClick = () => setShowResults(!showResults)
+  console.log(showResults)
+  console.log(props)
   return (
     <div>
-      <div onClick={onClick} > 인플루엔서 </div>
-      { showResults ? <Results /> : null }
+      <div className = {styles.rightallign} onClick={onClick} > 인플루엔서 </div>
+      { showResults ? <Results props={props}/> : null }
     </div>
   )
 }
 
-const Results = () => (
-  <Card sx={{ maxWidth: 1150, lineHeight: "109px"}}>
-    Some Results in this section
-  </Card>
-)
+const Results = (props) => {
+  console.log(props)
+  return (
+    <div>
+    <Card sx={{ width: 800, lineHeight: "50px", border: 1, borderColor: 'grey.200', my:"100px" }}>
+      <Container> 여행 스타일SNS 핫플 매니아 </Container>
+      <Container> 여행 스타일SNS 핫플 매니아 </Container>
+      <Container> 자세히 보기 </Container>
+    </Card>
+    </div> 
+  )
+}
 
 export default function Home() {
   const [showResults, setShowResults] = useState(false)
@@ -148,7 +142,10 @@ export default function Home() {
       <Search/>
       <Card sx={{ maxWidth: 1150, lineHeight: "109px"}}>
        <Container sx={{ display: 'flex' }}>
-          <Container sx={{ fontSize: '1.6rem', fontWeight : "700", display: 'flex'}}> <div>인플루언서 </div> <img onClick = {influencer_icon} className = {styles.influencer_icon} src="/static/images/avatar/icon_img.png"/> </Container>
+          <Container sx={{ fontSize: '1.6rem', fontWeight : "700", display: 'flex'}}> 
+          <div>인플루언서 </div> 
+          <img onClick = {influencer_icon} className = {styles.influencer_icon} src="/static/images/avatar/icon_img.png"/> 
+          </Container>
           <Container sx={{ color: "#0c43b7", fontSize: '1.3rem' }} className = {styles.contenttitle_rightend}> <Link href = "https://in.naver.com/"> 서비스 홈 바로가기 </Link> </Container>
         </Container>
       </Card>
@@ -166,10 +163,19 @@ export default function Home() {
           />
           <div className = {styles.right}> <img onClick = {addblogfan} className = {styles.fanicon} src="../static/images/avatar/fanicon.png"/> </div>
         </Container>
-        <Container sx={{ml:'40px' }}> <img className = {styles.blogicon} src="../static/images/avatar/blogicon.png"/> <ui className = {styles.subheader}> {each.blogdetails} </ui> <ui className = {styles.detailsdepth}> {each.detailsdepth} </ui> </Container>
-        <Link href={each.title_content_url} underline="none"> <Container sx={{my: '10px', color: '#0c43b7', fontSize: '1.6rem', ml:'40px' }} className = {styles.contenttitle}> {each.title} </Container> </Link>  
+
+        <Container sx={{ml:'40px', display : 'flex' }}> 
+          <img className = {styles.blogicon} src="../static/images/avatar/blogicon.png"/> 
+          <div className = {styles.subheader}> {each.blogdetails} </div> 
+          <div className = {styles.detailsdepth}> {each.detailsdepth} </div> 
+        </Container>
+        <Search props = {each.detailsdepth}/>
+        
+        <Link href={each.title_content_url} underline="none"> 
+        <Container sx={{my: '10px', color: '#0c43b7', fontSize: '1.6rem', ml:'40px' }} className = {styles.contenttitle}> {each.title} </Container> 
+        </Link>  
   
-        <Container sx={{ display: 'flex' }}>
+        <Container className = {styles.flex_allign}>
           <Container>
             <Link href={each.title_content_url} underline="none"> <Container sx={{my: '10px', color: "#404040", fontSize: '1.3rem' }}> {each.content} </Container> </Link>
             <Container sx={{my: '10px', color: 'grey', fontSize: '1.1rem' }}> {each.date} </Container>
