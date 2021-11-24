156d1a82df72027f9bbd3222467054e38d0a15c3
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-11-24 21:48:50 +0900

    Solve small error due to changed name of component

--- render_server/src/components/ViewCardBasic.js
+++ render_server/src/components/ViewCardBasic.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from '@mui/material/Link';
 import styles from './postCard.module.css'
 // import Image from "next/image"
-import Morevert from './Morevert'
+import MoreVert from './MoreVert'
 import DateConvertor from './DateConvertor'
 import Image from 'material-ui-image'
 
@@ -73,7 +73,7 @@ export default function ViewCardBasic({props, view}) {
                     </div>
                 }
                 action = {
-                    <Morevert props = {{
+                    <MoreVert props = {{
                         "open": open, 
                         "anchorEl": anchorEl, 
                         "handleClick": handleClick, 
