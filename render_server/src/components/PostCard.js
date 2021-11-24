b4438a34e41e7f4862aee6d6f967bf166f6dcb04
mhk-temp <isa000@kaist.ac.kr>
2021-11-24 14:47:35 +0900

    1. Make DB server and change json data, 2. Make page in render server without Post request(only for test), 3. Make different post request by id in web_server(basic id is Paris).

--- render_server/src/components/PostCard.js
+++ render_server/src/components/PostCard.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Link from '@mui/material/Link';
 import styles from './postCard.module.css'
-import Morevert from './Morevert'
+import MoreVert from './morevert'
 import DateConvertor from './DateConvertor'
 import Image from 'material-ui-image'
 
 import { Card, CardContent, CardHeader, Divider } from '@mui/material';
+
+var ViewType = Object.freeze({
+    NEWS: 0,
+    WIKI: 1
+});
+// typeScript 에서는 바꾸기 https://engineering.linecorp.com/ko/blog/typescript-enum-tree-shaking/
 
 export default function PostCard({props, view}) {  
     const { publisherURL, publisherImgURL, publisher, date, postURL, title, contents, contentsImgURL } = props
@@ -24,12 +30,20 @@ export default function PostCard({props, view}) {
         setAnchorEl(null);
     };
 
+    // var isWiki = true;
+    // var isWiki = false;
+    // var viewType = ViewType.NEWS;
+    // var viewType = ViewType.WIKI;
+    // console.log(viewType)
+    // console.log(view)
+    // console.log(props)
+
     return (
         <Card sx={{ maxWidth: 766 }} square > 
             <CardHeader 
                 sx={{ m: 0, p:0, paddingTop: '8px', paddingLeft: '16px', paddingRight: '8px' }} variant="contained"
                 avatar={
-                    <Link href={publisherURL}>
+                    <Link href={publisherURL} underline="none">
                         <a>
                             <div className={styles.publisherImgBoarder}>
                                 {publisherImgURL == ""
@@ -56,12 +70,13 @@ export default function PostCard({props, view}) {
                 }
                 title = {
                     <div className={styles.publishInfo}>
-                        <Link href={publisherURL}>
+                        <Link href={publisherURL} underline="none">
                             <a>
                                 <div className={styles.publisher}>{publisher}</div>
                             </a>
                         </Link>
-                        { viewType != "WIKI" &&
+                        { viewType != ViewType.WIKI &&
+                        // { viewType != "WIKI" &&
                             <hr className={styles.verticalDivider}></hr>
                         }
                         { date != "" &&
@@ -70,7 +85,7 @@ export default function PostCard({props, view}) {
                     </div>
                 }
                 action = {
-                    <Morevert props = {{
+                    <MoreVert props = {{
                         "open": open, 
                         "anchorEl": anchorEl, 
                         "handleClick": handleClick, 
@@ -80,14 +95,17 @@ export default function PostCard({props, view}) {
                 }
             />
             <CardContent sx={{ m: 0, p: 0, paddingLeft: '16px', paddingRight: '16px', marginBottom: '10px' }} variant="contained" >
-                <Link href={postURL}>
+                <Link href={postURL} underline="none">
                     <a>
                         <div className={styles.title}>{title}</div>
-                        { viewType == "WIKI" && <Divider 
+                        { viewType == ViewType.WIKI && <Divider 
+                        // { viewType == "WIKI" && <Divider 
                             sx={{mt: 1.25, mb: 1.25, color: 'gray.light' }} // theme.spacing value (the default for the value is 8px
                         />}
                         <div className={styles.contentsInfo}>
-                            <div className={styles.contents}>{contents}</div>
+                            <div className={styles.contents}>
+                                <p>{contents}</p>
+                            </div>
                             {contentsImgURL != "" && <div className={styles.contentsImgBoarder}>
                                 <Image className={styles.contentsImg}
                                     src={contentsImgURL}
--- render_server/src/components/wiki.js
+++ render_server/src/components/wiki.js
@@ -2,6 +2,11 @@ import PostCard from '../components/PostCard'
 import { Box } from '@mui/material';
 import MoreContent from '../components/MoreContent';
 
+var ViewType = Object.freeze({
+    NEWS: 0,
+    WIKI: 1
+});
+
 export default function Wiki({props}) {
     const {wiki_posts, wiki_more} = props
     return (
@@ -10,7 +15,7 @@ export default function Wiki({props}) {
             (wiki) => 
                 <div key={wiki.publisherURL} >
                     <Box sx={{mb: 1.25}}>
-                        <PostCard props={wiki} view={{ "viewType": "WIKI" }}/>
+                        <PostCard props={wiki} view={{ "viewType": ViewType.WIKI }}/>
                     </Box>
                 </div>
         )}
