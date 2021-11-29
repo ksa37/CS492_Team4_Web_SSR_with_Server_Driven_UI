8cb84642690c8ebaa031a20ac558b133b3e04f87
mhk-temp <isa000@kaist.ac.kr>
2021-11-29 15:43:50 +0900

    1. Merge with web/part and dev

--- render_server/src/pages/index.js
+++ render_server/src/pages/index.js
@@ -11,7 +11,8 @@ import Wiki from '../components/wiki';
 import View from '../components/view'
 import Influencer from '../components/influencer';
 import Drama from '../components/Drama';
-import ViewCardBasic from '../components/ViewCardBasic';
+import Bulguksa from '../components/Bulguksa';
+import Linkbox from '../components/Linkbox';
 
 const themeLight = createTheme({
   palette: {
@@ -32,36 +33,9 @@ const themeDark = createTheme({
   }
 });
 
-// export async function getServerSideProps(context) {
-//   const res = await fetch('http://localhost:5000/keywords')
-//   const data = await res.json()
-//   const string_data = JSON.stringify(data)
-//   const props = {data: ''}
-//   props.data = string_data
-//   /*
-//   const {req, } = context
-//   const props = {data: ''}
-//   if (req.method === "POST") {
-//     const streamPromise = new Promise((resolve, reject) => {
-//       let body = ''
-//       req.on('data', ( data ) => {
-//         body += data
-//       });
-//       req.on('end', () => {
-//         resolve(body);
-//       });
-//     });
-//     const body = await streamPromise;
-//       props.data = body;
-//   }
-//   return { props }
-//   */
-//   return { props }
-// }
-
 export async function getServerSideProps(context) {
   const {req, } = context
-  const props = {data: ''}
+  const props = {data: '', views: []}
   if (req.method === "POST") {
     const streamPromise = new Promise((resolve, reject) => {
       let body = ''
@@ -69,77 +43,93 @@ export async function getServerSideProps(context) {
         body += data
       });
       req.on('end', () => {
-        resolve(JSON.parse(body).data);
+        resolve(JSON.parse(body));
       });
     });
-    const body = await streamPromise;
-    props.data = body;
+    const json = await streamPromise;
+    props.data = json.data;
+    props.views = json.views;
   }
   else
   {
-    const res = await fetch('http://localhost:5000/Paris')
+    const res = await fetch('http://localhost:5000/Bulguksa')
     const data = await res.json()
     const string_data = JSON.stringify(data)
     props.data = string_data
+    props.views = data.views;
   }
   return { props }
 }
 
-
-// export default function Home({data}) {
-//   const json = JSON.parse(JSON.parse(data).data)
-//   // const json = JSON.parse(data)
-//   const news_view =  json[1].view.includes("news");
-//   const photo_view = json[1].view.includes("photo");
-//   const view_view = json[1].view.includes("review");
-//   const wiki_view = json[1].view.includes("wiki");
-export default function Home({ data }) {
+export default function Home({ data, views}) {
   const json = JSON.parse(data)
-  // const view_view = json.view.includes("review");
-  const news_view = json.view.includes("news");
-  const photo_view = json.view.includes("photo");
-  const wiki_view = json.view.includes("wiki");
-  const influencer_view = json.view.includes("influencer");
+  const keyword = json.keyword
   return (
     <ThemeProvider theme={themeLight}>
       <CssBaseline />
       <div className={styles.container}>
-        <div className="section_drama">
-          <Drama/>  
+        <div className="section_link">
+          <Linkbox />
         </div>
-        {/* <ViewCardBasic props={json.review}/> */}
-        {/* {view_view&&
-        <div className="section_view">
-        <View props={json.review}/>
-        </div>
-        } */}
-        {news_view && 
-        <div className="section_news">
-          <News props={json.news}/>
-        </div>}
-        {wiki_view && 
-        <div className="section_wiki">
-          <Wiki props={json.wiki}/>
-        </div>}
-        {photo_view&&
-        <div className="section_image">
-          <Photo props={json.photo}/>  
-        </div>}
-        {influencer_view&&
-        <div className="section_influencer">
-          <Influencer props={json.influencer}/>    
-        </div>}
-        <Box sx={{ mb: 15 }}></Box>
-        <Typography style={{ fontSize: 16, fontWeight: '900', verticalAlign: 'center', textAlign: 'center' }}>
-          <img src="/icons/naver_logo.png" 
-          width="auto"
-          height="21px"
-        />
-        </Typography>
-        <Typography style={{ fontSize: 14, fontWeight: '900', color: 'var(--date_gray)', verticalAlign: 'center', textAlign: 'center' }}>
-          Team 4
-        </Typography>
-        <Box sx={{ mb: 5 }}></Box>
+        {views.map((view, index) => {
+          switch(view) {
+            case "basic":
+              if (keyword === '불국사')
+              {
+                return (
+                  <div key={index}>
+                    <div className="section_bulguksa">
+                      <Bulguksa props={json.basic}/>
+                    </div>
+                    <div className="section_drama">
+                      <Drama/>  
+                    </div>
+                  </div>
+                )
+              }
+              break;
+            case "influencer":
+              return (
+                <div key={index} className="section_influencer">
+                  <Influencer props={json.influencer}/>
+                </div>
+              )
+            case "review":
+              return (
+                <div key={index} className="section_review">
+                  <View props={json.review}/>
+                </div>
+              )
+            case "wiki":
+              return (
+                <div key={index} className="section_wiki">
+                  <Wiki props={json.wiki}/>
+                </div>
+              )
+            case "news":
+              return (
+                <div key={index} className="section_news">
+                  <News props={json.news}/>
+                </div>
+              )
+            case "photo":
+              return (
+                <div key={index} className="section_photo">
+                  <Photo props={json.photo}/>
+                </div>
+              )
+          }})}
+          <Box sx={{ mb: 15 }}></Box>
+          <Typography style={{ fontSize: 16, fontWeight: '900', verticalAlign: 'center', textAlign: 'center' }}>
+            <img src="/icons/naver_logo.png" 
+            width="auto"
+            height="21px"
+          />
+          </Typography>
+          <Typography style={{ fontSize: 14, fontWeight: '900', color: 'var(--date_gray)', verticalAlign: 'center', textAlign: 'center' }}>
+            Team 4
+          </Typography>
+          <Box sx={{ mb: 5 }}></Box>
       </div>
     </ThemeProvider>
   )
