a0c6807de6cb2f3e73b448daeb9e39603008fec5
Zieho Choi <jeff426@naver.com>
2021-12-03 15:43:07 +0900

    main content, fan icon, flip page css

--- render_server/src/components/influencer.js
+++ render_server/src/components/influencer.js
@@ -1,7 +1,5 @@
-import { Container, Card, Grid, Link} from '@mui/material';
-// import Card from '@mui/material/Card';
+import { Container, Card, Grid, Link, Paper} from '@mui/material';
 import { useState } from 'react';
-// import Link from '@mui/material/Link';
 import styles from './influencer.module.css'
 import MoreContent from '../components/MoreContent';
 
@@ -11,8 +9,8 @@ const Blogfan = () => {
   return (
     <div>
       { showResults ? 
-      <div onClick={onClick} > <img className = {styles.fanicon} src="/images/influencer/unfan.png" alt='unfan'/>  </div> 
-      : <div onClick={onClick} > <img className = {styles.fanicon} src="/images/influencer/fanicon.png" alt='fan'/>  </div>}
+      <div onClick={onClick} > <img className = {styles.fanicon_after} src="/images/influencer/unfan.png" alt='unfan'/>  </div> 
+      : <div onClick={onClick} > <img className = {styles.fanicon_before} src="/images/influencer/fanicon.png" alt='fan'/>  </div>}
     </div>
   )
 }
@@ -32,14 +30,13 @@ const Search = (props) => {
 
 const Results = (props) => {
   return (
-    <Card sx={{ width: 700, lineHeight: "20px", border: 1, borderColor: 'grey.100', ml: "40px", mr: "40px", mb: "5px" }}>
-      <Card sx={{ width: 680, lineHeight: "20px", border: 1, borderColor: 'grey.100', ml: "10px", mr: "10px", fontSize: '0.8rem' }}>
-        <div className = {styles.typ_grey}> 여행 스타일:   <div className = {styles.typ_black}>{props.props.props[0]}</div> </div>
-        <div className = {styles.typ_grey}> 창작 분야:     <div className = {styles.typ_black}>{props.props.props[1]}</div></div>
-      </Card>
-      <div className = {styles.flipscreen}> 자세히 보기
-      </div>
-    </Card>
+    <Paper sx={{ width: 730, lineHeight: "20px", border: 1, borderColor: 'grey.100', ml: "18px", mr: "18px", mb: "5px", mt: "5px" }}>
+      <Grid sx={{ width: 710, lineHeight: "20px", border: 1, borderColor: 'grey.100', ml: "15px", mr: "10px", fontSize: '0.8rem', mt: "5px" }}>
+        <div className = {styles.typ_grey}> 여행 스타일:<div className = {styles.typ_black}>{props.props.props.details[0]}</div> </div>
+        <div className = {styles.typ_grey}> 창작 분야:  <div className = {styles.typ_black}>{props.props.props.details[1]}</div></div>
+      </Grid>
+      <Link href = {props.props.props.url}> <div className = {styles.flipscreen}> 자세히 보기 </div> </Link>
+    </Paper>
   )
 }
 
@@ -49,46 +46,52 @@ export default function Influencer(props) {
   return (
     <>
     <Card sx={{ maxWidth: 766, lineHeight: "50px"}}>
-      <Grid sx={{ display: 'flex' }}>
-          <div className = {styles.influencer}> 인플루언서 </div> 
-          <img className = {styles.influencer_icon} src="/images/influencer/icon_img.png" alt='icon'/>
-          <Link sx={{ color: "#0c43b7", fontSize: '15px', ml: "500px"}} href = "https://in.naver.com/"> 서비스 홈 바로가기 </Link> 
-      </Grid>
+      <div className = {styles.influencer}> 인플루언서 </div> 
+      <img className = {styles.influencer_icon} src="/images/influencer/icon_img.png" alt='icon'/>
+      <Link className = {styles.service} href = "https://in.naver.com/"> 서비스 홈 바로가기 </Link> 
     </Card>
 
     {data.map((each) => 
       <Card sx={{ maxWidth: 768}} key = {each.name}>
-        <Container sx={{ maxWidth: 768, mt:"10px" }}>
-          <Link href={each.avatar_name_url}> 
-          <img className = {styles.avatar} src={"/images/influencer/avatar_" + keyword + "_" + each.key + ".png"} alt={each.key}/>
-          </Link>
-            <div className = {styles.flex}> 
-              <Link href={each.avatar_name_url} underline="none"> <div className = {styles.title}>{each.name}</div> </Link> 
-              <div className = {styles.header}>{each.fans}</div> 
-            </div>  
-            <div className = {styles.header_sub}>{each.type}</div> 
-            <div className = {styles.header_sub_lft}>{each.place}</div>
-            <Blogfan/>
-        </Container>
+        <Grid sx={{ maxWidth: 768, mt:"10px", ml:"18px" }}>
+          <Link href={each.avatar_name_url}> <img className = {styles.avatar} src={"/images/influencer/avatar_" + keyword + "_" + each.key + ".png"} alt={each.key}/> </Link>
+          <div className = {styles.flex}> 
+            <Link href={each.avatar_name_url} underline="none"> <div className = {styles.title}>{each.name}</div> </Link> 
+            <div className = {styles.header}>{each.fans}</div> 
+          </div>
+          <div className = {styles.header_sub}>{each.type}</div> 
+          <div className = {styles.header_sub_lft}>{each.place}</div>
+          <Blogfan/>
+        </Grid>
 
-        <Container sx={{ml:'40px', display : 'flex' }}> 
+        <Container style = {{display: "flex"}}> 
           <img className = {styles.blogicon} src="/images/influencer/blogicon.png" alt='icon'/> 
           <div className = {styles.subheader}> {each.blogdetails} </div> 
-          <div className = {styles.detailsdepth}> {each.detailsdepth} </div> 
+          <div className = {styles.detailsdepth}> {each.detailsdepth} </div>
+          {/* <Search props = {each.detailsdepth}/> */}
         </Container>
-        <Search props = {each.detailsdepth}/>
+
+        <Search props = {{"details": each.detailsdepth, "url": each.avatar_name_url}}/>
         
         <Link href={each.title_content_url} underline="none"> 
-        <Container sx={{color: '#0c43b7', fontSize: '1rem', ml:'45px' }} className = {styles.contenttitle}> {each.title} </Container> 
+        <Grid sx={{color: '#0c43b7', fontSize: '1rem', ml:'20px' }} className = {styles.contenttitle}> {each.title} </Grid> 
         </Link>  
   
-        <Container sx={{ display : 'flex' }}>
-          <Container>
-            <Link href={each.title_content_url} underline="none"> <Container sx={{my: '10px', color: "#404040", fontSize: '1rem' }}> {each.content} </Container> </Link>
+        <div className = {styles.f}>
+          <div>
+            <Link href={each.title_content_url} underline="none"> <div className = {styles.content}> {each.content} </div> </Link>
             <Container sx={{my: '10px', color: 'grey', fontSize: '1rem' }}> {each.date} </Container>
-          </Container>
-          <Link href={each.title_content_url} underline="none"> <img className = {styles.image} src={"/images/influencer/blog_" + keyword + "_" + each.key + ".png"} alt={each.key}/> </Link>
-        </Container>
+          </div>
+          <Link href={each.title_content_url} underline="none"> <img className = {styles.image} src={"/images/influencer/blog_" + keyword + "_" + each.key + ".png"} alt={each.key}/> </Link> 
+        </div>
+
+        {/* <div className = {styles.f}> */}
+            {/* <div className = {styles.l}>L</div>
+            <div className = {styles.r}>R</div>
+            <div> L2 </div>
+            <div className = {styles.t}>T</div>
+            <div className = {styles.b}>B</div> */}
+        {/* </div> */}
       </Card>)}
       <MoreContent props={{'view_option': "인플루엔서",'more_link':morecontent}}/>
     </>
