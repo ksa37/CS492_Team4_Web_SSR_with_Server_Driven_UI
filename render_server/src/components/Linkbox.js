c2a0b2387d2da02e4297f11eeac4436c7e3cf6bf
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-12-06 22:07:45 +0900

    Delete not working code

--- render_server/src/components/Linkbox.js
+++ render_server/src/components/Linkbox.js
@@ -6,63 +6,6 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import styles from './SelectBox.module.css'
-// import { makeStyles } from '@mui/styles';
-
-// const useStyles = theme => ({
-//   root: {
-//     "& .MuiOutlinedInput-input": {
-//       color: "green"
-//     },
-//     "& .MuiInputLabel-root": {
-//       color: "green"
-//     },
-//     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-//       borderColor: "green"
-//     },
-//     "&:hover .MuiOutlinedInput-input": {
-//       color: "red"
-//     },
-//     "&:hover .MuiInputLabel-root": {
-//       color: "red"
-//     },
-//     "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-//       borderColor: "red"
-//     },
-//     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
-//       color: "purple"
-//     },
-//     "& .MuiInputLabel-root.Mui-focused": {
-//       color: "purple"
-//     },
-//     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-//       borderColor: "purple"
-//     }
-//   },
-// //   overrides: {
-// //     MuiSelect: {
-// //   select: {
-// //       '&:before': {
-// //           // borderColor: color,
-// //       },
-// //       '&:after': {
-// //           borderColor: "var(--naver_green)",
-// //       },
-// //       "&:focus": {
-// //         backgroundColor: "var(--naver_green)",
-// //         borderColor: "var(--naver_green)"
-// //       },
-// //   },
-// // }
-// // }
-// });
-
-// const useStyles = makeStyles({
-//   customOutline: {
-//     "& .MuiOutlinedInput-notchedOutline": {
-//       borderColor: "var(--naver_green)"
-//     }
-//   }
-// });
 
 export default function BasicSelect() {
   const [href, sethref] = React.useState('');
@@ -70,17 +13,12 @@ export default function BasicSelect() {
   const handleChange = (event) => {
     sethref(event.target.value);
   };
-  
-  // const classes = useStyles();
 
   return (
     <Box className={styles.wrap} sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
-       {/* classes={{ root: classes.customOutline }}> */}
         <InputLabel id="demo-simple-select-label">Select the keyword</InputLabel>
         <Select
-          // className={classes.select}
-          // className={classes.root}
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={href}
