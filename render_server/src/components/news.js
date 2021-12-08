ffe15c3c975a603c55ead71b527d64d1fe839f49
Jiyun Park <wldbs0507@kaist.ac.kr>
2021-12-08 16:50:48 +0900

    Add news unit test and screenshots

--- render_server/src/components/news.js
+++ render_server/src/components/news.js
@@ -73,8 +73,8 @@ export default function News({props}) {
             style={{ textAlign: 'left'}}
         />
         {news_posts&&news_posts
-          .map((news) => 
-          <PostCard key={news.publisherURL} props={news} view={{"viewType": ViewType.NEWS }}/>)}
+          .map((news, index) => 
+          <PostCard key={news.publisherURL} props={news} view={{"viewType": ViewType.NEWS }} index={index}/>)}
       </Card>
       <MoreContent props={{'view_option': "뉴스",'more_link':news_more}}/>
       </>
